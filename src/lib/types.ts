6aeeabebdd9fd246c1717bee7a32dba40761e494
Pedro Ferreira <pedrohsferreira1@gmail.com>
2024-09-15 22:56:22 -0300

    Fix rectangle movement to account for mouse offset during drag

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -3,13 +3,15 @@ import type { Options } from 'roughjs/bin/core';
 import type { ShapeKind } from './consts';
 
 export interface DesenhucaShape {
+	x: number;
+	y: number;
 	customize(options: DesenhucaShapeOptions): void;
-	move(x: number, y: number): void;
+	move(x: number, y: number, offset_x: number, offset_y: number): void;
 	resize(x: number, y: number): void;
 	draw(rough: RoughCanvas): void;
 	intersects(point: Point): boolean;
 	contains(point: Point): boolean;
-	coords(): Point[];
+	dimensions(): Point[];
 	highlight(rough: RoughCanvas): void;
 }
 
