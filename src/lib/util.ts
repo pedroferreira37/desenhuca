fa9ac12f8e6d97eaca56acc08e1de6e245413d31
Pedro Ferreira <pedrohsferreira1@gmail.com>
2024-10-17 23:44:06 -0300

    added normalize for initial inversed draw

--- src/lib/util.ts
+++ src/lib/util.ts
@@ -1,6 +1,6 @@
 import type { Tools } from './types';
 
-export function switchToolByKey(key: string): Tools {
+export function select_tool_by_key(key: string): Tools {
 	switch (key) {
 		case '1':
 			return 'pointer';
@@ -10,11 +10,15 @@ export function switchToolByKey(key: string): Tools {
 			return 'rectangle';
 		case '4':
 			return 'ellipse';
-		case '4':
+		case '5':
 			return 'line';
 		case '6':
 			return 'erase';
 		default:
 			return 'pointer';
 	}
+}
+
+export function match(value: string, pattern: string): boolean {
+	return value === pattern;
 }
